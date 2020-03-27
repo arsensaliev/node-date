23738eaa98ca4b09bc4eef5143f791704267ab14
Arsen Saliev <arsensaliev03@gmail.com>
2020-03-27 18:43:43 +0600

    fix write

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 //"Tue, 24 Mar 2020 09:30:28 GMT"
 const express = require("express");
 const app = express();
-const router = express.Router();
 const delay = process.env.DELAY;
 const stopTime = process.env.STOP_TIME;
 
-router.get("/users", (req, res, next) => {
+app.get("/users", (req, res, next) => {
     res.setHeader("Content-type", "text/html; charset=utf8");
     res.setHeader("Transfer-Encoding", "chunked");
     const logs = setInterval(() => {
@@ -18,18 +17,6 @@ router.get("/users", (req, res, next) => {
     }, stopTime);
 });
 
-router.get("/", (req, res, next) => {
-    res.setHeader("Content-type", "text/html; charset=utf8");
-    res.setHeader("Transfer-Encoding", "chunked");
-    const logs = setInterval(() => {
-        console.log(new Date().toUTCString());
-    }, delay);
-
-    setTimeout(() => {
-        clearInterval(logs);
-        res.send(new Date().toUTCString());
-    }, stopTime);
-});
 app.listen(3000, () => {
     console.log("Start localhost on port 3000");
 });
